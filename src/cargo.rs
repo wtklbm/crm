67bbb1c902e296c5740c5c2448204bcde6db347b
wtklbm <wtklbm@gmail.com>
2021-07-01 16:50:59 +0800

    🦄 refactor: 修改提示信息

--- src/cargo.rs
+++ src/cargo.rs
@@ -14,7 +14,7 @@ use crate::{
     },
     description::RegistryDescription,
     toml::Toml,
-    util::{cargo_config_path, field_eprint, get_cargo_config},
+    util::{cargo_config_path, error_print, field_eprint, get_cargo_config},
 };
 
 /// `Cargo` 配置对象
@@ -51,7 +51,10 @@ impl CargoConfig {
                 CargoConfig { data: config }
             }
             Err(_) => {
-                eprint!("{} 文件解析失败，{}", CARGO_CONFIG_PATH, PLEASE_TRY);
+                error_print(format!(
+                    "{} 文件解析失败，{}",
+                    CARGO_CONFIG_PATH, PLEASE_TRY
+                ));
                 process::exit(-1);
             }
         }
